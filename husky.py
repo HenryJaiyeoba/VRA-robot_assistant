f58e8de3b44ec0fe541a46046334c7019b470915
HenryJaiyeoba <HenryJaiyeoba@gmail.com>
2025-04-23 01:19:46 +0300

    test

--- husky.py
+++ husky.py
@@ -1,4 +1,21 @@
 from huskylib import HuskyLensLibrary
+import time
 
-h1 = HuskyLensLibrary("I2C", "", address=0x32)
-print(h1.knock())
+hl = HuskyLensLibrary("I2C", "", address=0x32)
+# print(hl.knock())
+
+def warn_user():
+    print("Hey, please move I have somewhere to be!")
+
+
+try:
+    while True:
+        hl.requestAll()
+        objects = hl.blocks()
+        if objects:
+            print(f"Detected {len(objects)} objects in front")
+            warn_user()
+        else:
+            print("the path is clear...")
+except KeyboardInterrupt:
+    print("Interrupted by the Jaiye")
